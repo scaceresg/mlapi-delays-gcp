ded8834408b4a877c193bdd7c8c5bec68e641172
Sebastian Cáceres Gelvez <sebastian.caceres.g@gmail.com>
2024-04-22 00:32:02 +0100

    Model feature finished
    The commit includes the following:
    
    * exploration.ipynb was executed and analysed, and a performance summary was included at the end to make the final decision about the model selection.
    
    * The selected model was transcribed from exploration.ipynb to model.py inside the DelayModel class
    
    * Methods `get_min_diff()` and `get_delay()` were added to the DelayModel class and used in the preprocessing step.
    
    * The model was modified accordingly to pass the tests.

--- challenge/model.py
+++ challenge/model.py
@@ -1,6 +1,9 @@
+import numpy as np
 import pandas as pd
-
+from datetime import datetime
 from typing import Tuple, Union, List
+from sklearn.model_selection import train_test_split
+import xgboost as xgb
 
 class DelayModel:
 
@@ -8,12 +11,14 @@ class DelayModel:
         self
     ):
         self._model = None # Model should be saved in this attribute.
+        self.raw_data = pd.read_csv(filepath_or_buffer="./data/data.csv")
 
+    # Preprocess data: get features and target (if not None)
     def preprocess(
-        self,
-        data: pd.DataFrame,
-        target_column: str = None
-    ) -> Union(Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame):
+        self, 
+        data: pd.DataFrame, 
+        target_column: str=None
+    ) -> Union[Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame]:
         """
         Prepare raw data for training or predict.
 
@@ -26,8 +31,51 @@ class DelayModel:
             or
             pd.DataFrame: features.
         """
-        return
+        # Important features
+        top_features = [
+            "OPERA_Latin American Wings", 
+            "MES_7",
+            "MES_10",
+            "OPERA_Grupo LATAM",
+            "MES_12",
+            "TIPOVUELO_I",
+            "MES_4",
+            "MES_11",
+            "OPERA_Sky Airline",
+            "OPERA_Copa Air"
+        ]
+        
+        # Get dummies and concatenate
+        features = None
+        try:
+            features = pd.concat([
+                pd.get_dummies(data['OPERA'], prefix='OPERA'),
+                pd.get_dummies(data['TIPOVUELO'], prefix='TIPOVUELO'), 
+                pd.get_dummies(data['MES'], prefix='MES')], 
+                axis = 1)
+        except:
+            print("Input data is missing at least one of the columns: 'OPERA', 'MES', 'TIPOVUELO'")
 
+        # Filter top features
+        features = features[top_features]
+
+        # If target_column is None: return features
+        if target_column is None:
+            return features
+
+        elif target_column not in data.columns: # elif not in columns: find target ('delay')
+            try:
+                data_target = self.get_delay(data, threshold=15)
+                target = pd.DataFrame(data_target['delay'])
+            except:
+                print("Input data is missing at least one of the columns: 'Fecha-I', 'Fecha-O'")
+            return ([features, target])
+        
+        else:   # if in columns
+            target = pd.DataFrame(data[target_column])
+            return ([features, target])
+
+    # Fit model
     def fit(
         self,
         features: pd.DataFrame,
@@ -40,8 +88,27 @@ class DelayModel:
             features (pd.DataFrame): preprocessed data.
             target (pd.DataFrame): target.
         """
-        return
+        # Split data
+        X_train, _, y_train, _ = train_test_split(features, 
+                                                  target, 
+                                                  test_size=0.33,
+                                                  random_state=42)
+        # Get training data scale
+        n_y0 = len(y_train[y_train == 0].dropna())
+        n_y1 = len(y_train[y_train == 1].dropna())
+        scale = n_y0 / n_y1
 
+        # Define model with balanced classes
+        self._model = xgb.XGBClassifier(random_state=1, 
+                                     learning_rate=0.01,
+                                     scale_pos_weight=scale)
+        # Train model
+        self._model.fit(X_train, y_train)
+        print('Model has been trained successfully!')
+
+        return None
+
+    # Predict delay
     def predict(
         self,
         features: pd.DataFrame
@@ -55,4 +122,59 @@ class DelayModel:
         Returns:
             (List[int]): predicted targets.
         """
-        return
+        # Check if self._model is None
+        if self._model is None:
+            _features, _target = self.preprocess(data=self.raw_data,
+                                                 target_column='delay')
+            self.fit(features=_features, target=_target)
+            y_pred = self._model.predict(features)
+            y_pred_list = [int(y) for y in y_pred]
+        else:
+            # Predict for new flights
+            y_pred = self._model.predict(features)
+            y_pred_list = [int(y) for y in y_pred]
+
+        return y_pred_list
+    
+    ###### New Methods: Preprocessing methods ######
+    # Get minute difference
+    def get_min_diff(
+        self, 
+        data:pd.DataFrame
+    ) -> float:
+        """
+        Get minute difference between scheduled and operation dates.
+
+        Args:
+            data (pd.DataFrame): raw data.
+
+        Returns:
+            float: minute difference.
+        """
+        fecha_o = datetime.strptime(data['Fecha-O'], '%Y-%m-%d %H:%M:%S')
+        fecha_i = datetime.strptime(data['Fecha-I'], '%Y-%m-%d %H:%M:%S')
+        min_diff = ((fecha_o - fecha_i).total_seconds())/60
+        
+        return min_diff
+    
+    # Get delay column (target)
+    def get_delay(
+        self, 
+        data:pd.DataFrame, 
+        threshold:int=15
+    ) -> pd.DataFrame:
+        """Get delay column (target).
+
+        Args:
+            data (pd.DataFrame): raw data.
+            threshold (int, optional): time in minutes to consider a delay. 
+            Defaults to 15.
+
+        Returns:
+            pd.DataFrame: data with target column.
+        """
+        data['min_diff'] = data.apply(self.get_min_diff, axis=1)
+        data['delay'] = np.where(data['min_diff'] > threshold, 1, 0)
+
+        return data
+        
--- docs/challenge.md
+++ docs/challenge.md
@@ -1,5 +1,81 @@
-## LATAM Challenge Documentation
+# LATAM Challenge Documentation
 
 by Sebastian Cáceres Gelvez
 
-### 1. Exploration
+## Part I
+
+### 1. Model Selection
+
+Model selection consisted of the following steps:
+
+* First, I started by reviewing the `exploration.ipynb` notebook and 
+the analysis made by the DS.
+
+* While going through the notebook, I checked the `data` DataFrame every
+often to see how data was being handled/modified and to understand how it 
+was input to the model in the form of `features`.
+
+* In section **4. Training**, I went a bit further and produced
+a *Normalized Confusion Matrix* plot to more easily identify how each 
+model was performing. I repeated this step for the improved models in 
+section **6. Training with Improvement**.
+
+* I also saved the output from the `classification_report()` method for 
+each model and added the computation of the `roc_auc_score()` metric to it. 
+The information from the classification report allowed me to generate a 
+summary table with the performance metrics for the six proposed models, which
+is shown in section **8. Performance Summary** and below:
+
+    - First, I defined the `create_classif_summary()` function to convert the 
+    classification reports for all the models into a `df_summary` DataFrame.
+    
+    - Then, using this method, I input the classification reports and 
+    generated a summary table for their performance metrics. Below are some
+    conventions:
+
+        `xgb`: Simple XGBoost
+
+        `lreg`: Simple Logistic Regression
+
+        `xgb2-bal-topfeat`: XGBoost with Feature Importance and Balanced Data
+
+        `xgb3-nobal-topfeat`: XGBoost with Feature Importance but *without* Balanced Data
+
+        `lreg2-bal-topfeat`: Logistic Regression with Feature Importance and Balanced Data
+
+        `lreg3-nobal-topfeat`: Logistic Regression with Feature Importance but *without* 
+        Balanced Data
+
+        |                |      xgb | xgb2-bal-topfeat | xgb3-nobal-topfeat |      lreg | lreg2-bal-topfeat  | lreg3-nobal-topfeat |
+        |:---------------|---------:|-----------------:|-------------------:|----------:|-------------------:|--------------------:|
+        | 0_precision    | 0.812778 |        0.879198  |         0.813651   | 0.816599  |          0.878345  |           0.814335  |
+        | 1_precision    | 0        |        0.249248  |         0.764706   | 0.558952  |          0.247715  |           0.529412  |
+        | 0_recall       | 1        |        0.522357  |         0.999563   | 0.994479  |          0.518585  |           0.997376  |
+        | 1_recall       | 0        |      **0.68842** |         0.00616991 | 0.0303749 |        **0.688182**|           0.0128144 |
+        | accuracy       | 0.812778 |        0.553448  |         0.813577   | 0.813977  |          0.550338  |           0.813044  |
+        | 0_f1-score     | 0.896721 |        0.655351  |         0.897076   | 0.896803  |          0.65214   |           0.896609  |
+        | 1_f1-score     | 0        |      **0.365988**|         0.0122411  | 0.0576187 |        **0.364299**|           0.0250232 |
+        | w-avg_f1-score | 0.728835 |        0.601176  |         0.731415   | 0.739689  |          0.598249  |           0.733429  |
+        | roc-auc-score  | 0.5      |      **0.605388**|         0.502866   | 0.512427  |        **0.603384**|           0.505095  |
+
+* As the DS mentioned in the conclusions, the summary table confirmed that 
+there is not much difference between XGBoost and Logistic Regression models, 
+and the use of balanced classes improved the performance of the models in terms 
+of `1_recall`, `1_f1-score` and `roc-auc-score`. However, the XGBoost model 
+`xgb2-bal-topfeat` **performed slightly better** than the Logistic Regression model 
+`lreg2-bal-topfeat`.
+
+* Thus, the **final decision was to use the `xgb2-bal-topfeat` model**, i.e., the 
+*XGBoost with Feature Importance and Balanced Data* model
+
+### 2. Model Transcription
+
+Model transcription was carried out successfully. Two additional methods were 
+included in the `DelayModel` class for the *preprocessing* step:
+
+* `get_min_diff()`: Gets minute difference between scheduled (`'Fecha-I'`) and
+operation dates (`'Fecha-O'`).
+
+* `get_delay()`: Gets delay column (target) in case the argument `target_column`
+is given when calling the `preprocess()` method, but it is not a column in the 
+raw data.
